90c4629352ebc85e71462835e2e99885f745051a
Zhaoyu <weixiaobo88@gmail.com>
2020-10-27 20:05:42 +0800

    [TodoList][Linne] dev: init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MultiCounter from "./components/MultiCounter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MultiCounter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
